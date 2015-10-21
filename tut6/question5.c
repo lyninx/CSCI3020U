422ab1cb53f80a12e307b91ed1c4cbf0f7744a88
Kathryn McKay <kathryn.mckay@uoit.net>
2015-10-21 13:02:24 -0400

    Barriers substituting joins setup

--- tut6/question5.c
+++ tut6/question5.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <pthread.h>
 
 
@@ -6,32 +7,49 @@ static double total_grade = 0.0;
 static double total_bellcurve = 0.0;
 // barrier
 pthread_barrier_t read_grades_barr;
+pthread_barrier_t total_grade_barr;
+pthread_barrier_t total_bellcurve_barr;
+pthread_barrier_t bellcurve_file_barr;
 pthread_barrier_t save_bellcurve_barr;
 
 // ngrades
 #define N_GRADES 10
 
 // read in grades from file
-void read_grades(int* list)
+void read_grades(double* list)
 {
+	// todo
 	for (int i = 0; i < N_GRADES; i++)
 	{
 		list[i] = 0;
 	}
 
+	// signal
+	int barrier = pthread_barrier_wait(&read_grades_barr);
+	if (barrier != PTHREAD_BARRIER_SERIAL_THREAD && barrier != 0)
+		return 1;
+
 }
 
-void save_bellcurve(double grade)
+void save_bellcurve(double* grade)
 {
+
 	// record grade
-	total_grade += grade;
+	//total_grade += grade;
+	
+	printf("print grade %d\n", *grade);
+	//sleep(50);
+
 
 	//do bellcurve
-	grade *= 1.50;
-	total_bellcurve += grade*1.50;
+	*grade = *grade *1.50;
+	//total_bellcurve += grade*1.50;
 
 	// print bellcurve
 
+
+	// setup join
+	pthread_barrier_wait(&save_bellcurve_barr);
 
 }
 
@@ -39,19 +57,34 @@ void save_bellcurve(double grade)
 int main(int argc, char* argv[])
 {
 	// init list
-	int list[N_GRADES] = { 1 };
+	double grades[N_GRADES] = { 1.0 };
+	pthread_t save_bellcurve_thrs[N_GRADES];
 
-	// init barrier
-	pthread_barrier_init(&read_grades_barr, NULL, 1);
+	// init barriers
+	pthread_barrier_init(&read_grades_barr, NULL, 2);
+	pthread_barrier_init(&save_bellcurve_barr, NULL, 11);
 
 	// read grades
 	pthread_t thr;
-	pthread_create(&thr, 0, read_grades, (void *)list);
-
-	// join
+	pthread_create(&thr, 0, read_grades, (void *)grades);
+	
+	// wait for read grades to finish
 	int barrier = pthread_barrier_wait(&read_grades_barr);
 	if (barrier != PTHREAD_BARRIER_SERIAL_THREAD && barrier != 0)
 		return 1;
-    
+
+	// apply bellcurve
+	for (int i = 0; i < N_GRADES; i++)
+	{
+		pthread_create(&save_bellcurve_thrs[i], 0, save_bellcurve, (void *)&grades[i]);
+	}
+
+	// wait for bellcurve
+	barrier = pthread_barrier_wait(&save_bellcurve_barr);
+	if (barrier != PTHREAD_BARRIER_SERIAL_THREAD && barrier != 0)
+		return 1;
+
+	
+    // return
     return 0;
 }
