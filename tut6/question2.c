d4b9bb68fc73e73133c7fef25fb3792e599e31fc
Dennis Pacewicz <dennis.pacewicz@gmail.com>
2015-10-21 21:30:24 -0400

    finished q2 without locks

--- tut6/question2.c
+++ tut6/question2.c
@@ -4,15 +4,37 @@
 
 #define NUM_STUDENTS 5
 
+void *bellcurve(void* grade)
+{
+	int* gr = (int*)grade;
+	//do bellcurve
+	float result = (*gr) * 1.50;
+	printf("%f\n", result);
+
+	return 0;
+}
 int main(void)
 {
-	char students[NUM_STUDENTS];	
-	printf("Enter %d student names:\n", NUM_STUDENTS);
+	int grades[NUM_STUDENTS];
+	pthread_t t[NUM_STUDENTS];
 
+	printf("Enter %d student grades:\n", NUM_STUDENTS);
+
+	// Enter grades
 	for(int i=0; i < NUM_STUDENTS; i++){
-		scanf("%s", &students[i]);	
+		scanf("%d", &grades[i]);
+			
+	}
+	printf("Adjusted grades:\n");
+	// run bellcurve on a seperate thread for each student
+	for(int i=0; i < NUM_STUDENTS; i++){
+		pthread_create(&t[i], 0, bellcurve, (void *)&grades[i]);	
 	}
 	
+	// join threads
+	for(int i = 0; i < NUM_STUDENTS; i++){
+		pthread_join(t[i], 0);
+	}
 
 	return 0;
 }
