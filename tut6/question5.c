742b0c68f93b92c84101ead324c0b3314ab1f9f0
Kathryn McKay <kathryn.mckay@uoit.net>
2015-10-24 19:24:22 -0400

    Bascially done now; temoved the test output

--- tut6/question5.c
+++ tut6/question5.c
@@ -97,45 +97,31 @@ void save_bellcurve(double* grade)
 	double grade_val = *grade;
 
 	// record grade
-	printf("1[%lf] waiting to add grade\n", grade_val);
 	pthread_mutex_lock(&total_grade_lock);
-	printf("1[%lf] acquired grade lock\n", grade_val);
 	total_grade += grade_val;
-	printf("1[%lf] releasing grade lock\n", grade_val);
 	pthread_mutex_unlock(&total_grade_lock);
 
 
 	// wait around until the main thread recognizes what you did
-	printf("2[%lf] ready to report total_grade\n", grade_val);
 	pthread_mutex_lock(&grades_sum_barr_lock);
-	printf("2[%lf] reporting total_grade\n", grade_val);
 	pthread_barrier_wait(&grades_sum_barr);
-	printf("2[%lf] move on from total_grade\n", grade_val);
 	pthread_mutex_unlock(&grades_sum_barr_lock);
 
 	// apply bellcurve
 	double bellcurve_val = (bellcurve_val = grade_val*1.50f) > Q5_MAX_GRADE ? Q5_MAX_GRADE : bellcurve_val;
 
 	//do bellcurve
-	printf("3[%lf] waiting to add bellcurve\n", grade_val);
 	pthread_mutex_lock(&total_bellcurve_lock);
-	printf("3[%lf] acquired bellcurve lock\n", grade_val);
 	total_bellcurve += bellcurve_val;
-	printf("3[%lf] releasing bellcurve lock\n", grade_val);
 	pthread_mutex_unlock(&total_bellcurve_lock);
 
 	// wait around until the main thread recognizes what you did x2
-	printf("4[%lf] bellcurve report ready to\n", grade_val);
 	pthread_mutex_lock(&bellcurve_sum_barr_lock);
-	printf("4[%lf] bellcurve report doing it\n", grade_val);
 	pthread_barrier_wait(&bellcurve_sum_barr);
-	printf("4[%lf] bellcurve report passing baton\n", grade_val);
 	pthread_mutex_unlock(&bellcurve_sum_barr_lock);
 
 	// print bellcurve
-	printf("5[%lf] save bellcurve: ready to write to file\n", grade_val);
 	pthread_mutex_lock(&bellcurve_file_lock);
-	printf("5[%lf] save bellcurve: opening up file for writing\n", grade_val);
 	FILE* bellcurvef = fopen(FPATH_BELLCURVE, "a");
 	if (!bellcurvef)
 	{
@@ -146,14 +132,11 @@ void save_bellcurve(double* grade)
 		fprintf(bellcurvef, "%lf\n", bellcurve_val);
 	}
 	fclose(bellcurvef);
-	printf("5[%lf] save bellcurve: finished saving bellcurve\n", grade_val);
 	pthread_mutex_unlock(&bellcurve_file_lock);
 
 
 	// setup join
-	printf("6[%lf] My work here is done\n", grade_val);
 	pthread_barrier_wait(&bellcurve_saved_barr);
-	printf("passed barrier\n");
 
 }
 
@@ -197,14 +180,12 @@ int main(int argc, char* argv[])
 	// wait for grades
 	for (int i = 0; i < Q5_N_GRADES; i++)
 	{
-		printf("[main]waiting for grades from %d\n", i);
 		barrier = pthread_barrier_wait(&grades_sum_barr);
 		if (barrier != PTHREAD_BARRIER_SERIAL_THREAD && barrier != 0)
 			return 1;
 	}
 
 	// get average for grades (humoring the mutex lock respectfully)
-	printf("[main] calculating grade average...\n");
 	pthread_mutex_lock(&total_grade_lock);
 	grades_average = total_grade / (double)Q5_N_GRADES;
 	pthread_mutex_unlock(&total_grade_lock);
@@ -212,27 +193,23 @@ int main(int argc, char* argv[])
 	// wait for bellcurve
 	for (int i = 0; i < Q5_N_GRADES; i++)
 	{
-		printf("[main]waiting for bellcurve from %d\n", i);
 		barrier = pthread_barrier_wait(&bellcurve_sum_barr);
 		if (barrier != PTHREAD_BARRIER_SERIAL_THREAD && barrier != 0)
 			return 1;
 	}
 	
 	// get average for grades (humoring the mutex lock respectfully)
-	printf("[main] calculating bellcurved average...\n");
 	pthread_mutex_lock(&total_bellcurve_lock);
 	bellcurve_average = total_bellcurve / (double)Q5_N_GRADES;
 	pthread_mutex_unlock(&total_bellcurve_lock);
 
 	// wait for bellcurve to be saved to file
-	printf("[main] waiting to output results...\n");
 	barrier = pthread_barrier_wait(&bellcurve_saved_barr);
 	if (barrier != PTHREAD_BARRIER_SERIAL_THREAD && barrier != 0)
 		return 1;
 
 
 	// join all threads
-	printf("[main] waiting for threads to terminate...\n");
 	pthread_join(readgrades_thr, 0);
 	for (int i = 0; i < Q5_N_GRADES; i++)
 	{
@@ -241,7 +218,6 @@ int main(int argc, char* argv[])
 
 
 	// display stats
-	printf("[main] gonna put out the results...\n");
 	printf("Average before bellcurve: %lf\n", grades_average);
 	printf("Average after bellcurve: %lf\n", bellcurve_average);
 
