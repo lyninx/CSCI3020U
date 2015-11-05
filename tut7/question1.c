a4f50c910c4a3318560ad1182352cbaee82cf6b9
Dennis Pacewicz <dennis.pacewicz@gmail.com>
2015-11-05 11:04:00 -0500

    added queue things

--- tut7/question1.c
+++ tut7/question1.c
@@ -8,14 +8,39 @@
 #include <sys/wait.h>
 #include "queue.h"
 
+void push(node_t * head, proc p) {
+    node_t * current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
 
+    /* now we can add a new variable */
+    current->next = malloc(sizeof(node_t));
+    current->next->process = p;
+    current->next->next = NULL;
+}
+int pop(node_t ** head) {
+    int retval = -1;
+    node_t * next_node = NULL;
 
-void push(proc p){
-	queue q;
-	q.process = p;
-	q.next = NULL;
+    if (*head == NULL) {
+        return -1;
+    }
 
-	printf("%d", q.process);
+    next_node = (*head)->next;
+    retval = (*head)->process;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
+void print_list(node_t * head) {
+    node_t * current = head;
+
+    while (current != NULL) {
+        printf("%d\n", current->process);
+        current = current->next;
+    }
 }
 int main(void) {
 
@@ -26,7 +51,13 @@ int main(void) {
 	p.pid = 1;
 	p.runtime = 0;
 
-	push(p);
-	printf("test\n");
+	node_t * head = NULL;
+	head = malloc(sizeof(node_t));
+	if (head == NULL) {
+	    return 1;
+	}
+
+	head->process = p;
+	head->next = NULL;
 	
 }
