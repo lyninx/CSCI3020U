f6b7270d0f55e365b453eb5a8ae4c9591e9b61be
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-11 12:14:39 -0500

    trees are for hippies

--- tut8/question1.c
+++ tut8/question1.c
@@ -1,47 +1,19 @@
-#include "btree.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-void print_tree(const proc_tree* tree)
+typedef struct
 {
-	// print tree
-	if(tree != NULL)
-	{
-		// print data
-		printf("%s ", tree->value.name);
-
-		// print left
-		if(tree->left != NULL)
-		print_tree(tree->left);
-
-		// print right
-		if(tree->right != NULL)
-			print_tree(tree->right);
-	}
-
-	// print nothing
-	else
-		printf("<empty>\n");
-
-	
-
-}
+	char parent[256];
+	char name[256];
+	int priority;
+	int memory;
+} proc;
 
 int main(void)
 {
-	proc_tree* tree = (proc_tree*)malloc(sizeof(proc_tree));
-	proc process1;
-	strcpy(process1.name, "blue");
-
-	dfs(tree);
-	add_node(tree, &process1);
-
-	print_tree(tree);
-
-	clear_tree(&tree);
-
-	print_tree(tree);
+	
 
 	return 0;
 }
