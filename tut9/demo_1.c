6e5450135c18d97dfec8f19931ca474cc313a7b3
TTTruong <truyen.truong@uoit.net>
2015-11-18 12:05:21 -0500

    Completed question1.

--- tut9/demo_1.c
+++ tut9/demo_1.c
@@ -3,6 +3,8 @@
 int main (int argc, char *argv[])
 {
     printf("This statement will always print.\n");
+
+    /* Will only print out the statement if being compiled with -fopenmp. */
     #ifdef _OPENMP
     printf("This statement will only print when compiled with \"-fopenmp\"\n");
     #endif
