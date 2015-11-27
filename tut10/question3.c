d46e14eaa2afdc9107f76e44eefd82a2e98a4a03
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-27 14:47:31 -0500

    ...I just hate 2D arrays...>_>

--- tut10/question3.c
+++ tut10/question3.c
@@ -7,7 +7,7 @@
 
 // convenience print.
 // set it up to not flood console too badly.
-void print_matrix(int m[MATRIX_SIZE][MATRIX_SIZE], int nleft, int nright, int ntop, int nbot)
+void print_matrix(int m[], int nleft, int nright, int ntop, int nbot)
 {
 	// go through each row
 	for(int i = 0; i < MATRIX_SIZE; i++)
@@ -21,7 +21,7 @@ void print_matrix(int m[MATRIX_SIZE][MATRIX_SIZE], int nleft, int nright, int nt
 				// check if this row is in horizontal range
 				if(j < nleft || (MATRIX_SIZE- (j+1)) < nright)
 					// print cell
-					printf("\t%d", m[i][j]);
+					printf("\t%d", m[i*MATRIX_SIZE + j]);
 				else if(j == nleft)
 					// show that there are intermediate values
 					printf("\t...");
@@ -45,6 +45,11 @@ int main(int argc, char* argv[]){
 	// Initialize MPI environment
 	MPI_Init(&argc, &argv);
 
+
+	// Get the number of processes
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
 	// Get the rank of this process
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
@@ -53,27 +58,34 @@ int main(int argc, char* argv[]){
 	if(world_rank == 0)
 	{
 		// operand A
-		int A[MATRIX_SIZE][MATRIX_SIZE];
+		int A[MATRIX_SIZE*MATRIX_SIZE];
 
 		// operand B
-		int B[MATRIX_SIZE][MATRIX_SIZE];
+		int B[MATRIX_SIZE*MATRIX_SIZE];
 
 		// result matrix
-		int C[MATRIX_SIZE][MATRIX_SIZE];
+		int C[MATRIX_SIZE*MATRIX_SIZE];
 
 		// initialize matrices
 		for(int i = 0; i < MATRIX_SIZE; i++)
 		{
 			for(int j = 0; j < MATRIX_SIZE; j++)
 			{
-				A[i][j] = i;
-				B[i][j] = j;
-				C[i][j] = 0;
+				A[i*MATRIX_SIZE + j] = i;
+				B[i*MATRIX_SIZE + j] = j;
+				C[i*MATRIX_SIZE + j] = 0;
 			}
+		}
+
+		// broadcast each row of A
+		for(int i = 0; i < MATRIX_SIZE; i++)
+		{
+			//MPI_Send((void*)A[i], MATRIX_SIZE, MPI_INTEGER, i % world_size)
 		}
 
 		//todo
 
+		// print results
 		printf("[ MATRIX A ]:\n");
 		print_matrix(A, 4, 4, 4, 4);
 		printf("[ MATRIX B ]:\n");
