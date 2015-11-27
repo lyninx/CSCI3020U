3ce1cbbe69475bff02f07c599138831cd9d516e6
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-27 14:38:05 -0500

    removed that hello world stuff

--- tut10/question3.c
+++ tut10/question3.c
@@ -45,21 +45,9 @@ int main(int argc, char* argv[]){
 	// Initialize MPI environment
 	MPI_Init(&argc, &argv);
 
-	// get number of procs
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
 	// Get the rank of this process
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-	// Get processor name
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
-
-	// Print stuff
-	printf("processor %s, rank %d/%d\n", processor_name, world_rank, world_size);
 
 	// master process
 	if(world_rank == 0)
