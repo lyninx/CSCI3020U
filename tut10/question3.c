743ab8da0fa4b30861bc593d1491f5731f3abc3b
Kathryn McKay <kathryn.mckay@uoit.net>
2015-11-27 14:37:26 -0500

    brought my cute print function back

--- tut10/question3.c
+++ tut10/question3.c
@@ -5,6 +5,40 @@
 
 #define MATRIX_SIZE 100
 
+// convenience print.
+// set it up to not flood console too badly.
+void print_matrix(int m[MATRIX_SIZE][MATRIX_SIZE], int nleft, int nright, int ntop, int nbot)
+{
+	// go through each row
+	for(int i = 0; i < MATRIX_SIZE; i++)
+	{
+		// check if this row is in vertical range
+		if(i < ntop || (MATRIX_SIZE - (i + 1)) < nbot)
+		{
+			// print row
+			for(int j = 0; j < MATRIX_SIZE; j++)
+			{
+				// check if this row is in horizontal range
+				if(j < nleft || (MATRIX_SIZE- (j+1)) < nright)
+					// print cell
+					printf("\t%d", m[i][j]);
+				else if(j == nleft)
+					// show that there are intermediate values
+					printf("\t...");
+			}
+
+			// end line
+			printf("\n");
+		} else if (i == ntop) {
+			// show that there are unshown values
+			printf("\t ......\n");
+		}
+	}
+
+	// end list
+	printf("\n");
+}
+
 int main(int argc, char* argv[]){
 	
 	
@@ -51,6 +85,13 @@ int main(int argc, char* argv[]){
 		}
 
 		//todo
+
+		printf("[ MATRIX A ]:\n");
+		print_matrix(A, 4, 4, 4, 4);
+		printf("[ MATRIX B ]:\n");
+		print_matrix(B, 4, 4, 4, 4);
+		printf("[ MATRIX C ]:\n");
+		print_matrix(C, 4, 4, 4, 4);
 
 
 	}
